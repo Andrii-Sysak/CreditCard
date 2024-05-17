acc0db9854ad721232ad610752d1a45cfa65d46b
Andrii-Sysak <112658424+Andrii-Sysak@users.noreply.github.com>
2024-05-17 22:47:13 +0300

    Update preprocessing.py

--- services/preprocessing.py
+++ services/preprocessing.py
@@ -7,7 +7,51 @@ from category_encoders import LeaveOneOutEncoder
 from sklearn.preprocessing import StandardScaler
 from data_split import data_split_and_save
 
-from columns import delete_columns, scaling_data
+from columns import (
+    delete_columns, 
+    scaling_columns,
+    mean_impute_columns,
+    mode_impute_columns,
+    outlier_columns
+)
+
+
+def impute_na(df, variable, value) -> pd.DataFrame:
+    return df[variable].fillna(value)
+
+
+def input_missing_values(df: pd.DataFrame):
+    mean_impute_values = dict()
+    for column in mean_impute_columns:
+        mean_impute_values[column] = df[column].mean()
+        df[column] = impute_na(df, column, mean_impute_values[column])
+
+    mode_impute_values = dict()
+    for column in mode_impute_columns:
+        mode_impute_values[column] = df[column].mode()[0]
+        df[column] = impute_na(df, column, mode_impute_values[column])
+    
+    return df
+
+
+def find_skewed_boundaries(df, variable, distance):
+    df[variable] = pd.to_numeric(df[variable],errors='coerce')
+    IQR = df[variable].quantile(0.75) - df[variable].quantile(0.25)
+    lower_boundary = df[variable].quantile(0.25) - (IQR * distance)
+    upper_boundary = df[variable].quantile(0.75) + (IQR * distance)
+    return upper_boundary, lower_boundary
+
+
+def fix_anomalies(df: pd.DataFrame) -> pd.DataFrame:
+    upper_lower_limits = dict()
+    for column in outlier_columns:
+        upper_lower_limits[column+'_upper_limit'], upper_lower_limits[column+'_lower_limit'] = find_skewed_boundaries(df, column, 5)
+    for column in outlier_columns:
+        df = df[~ np.where(df[column] > upper_lower_limits[column+'_upper_limit'], True,
+                        np.where(df[column] < upper_lower_limits[column+'_lower_limit'], True, False))]
+    
+    return df
+
 
 def categorical_encoding(df: pd.DataFrame) -> pd.DataFrame:
     le = LabelEncoder
@@ -34,12 +78,12 @@ def data_scarling(df: pd.DataFrame) -> pd.DataFrame:
     scaler = StandardScaler()
 
     # fit the scaler to the train set, it will learn the parameters
-    scaler.fit(df[scaling_data])
+    scaler.fit(df[scaling_columns])
 
     # transform train and test sets
-    df_scaled = scaler.transform(df[scaling_data])
+    df_scaled = scaler.transform(df[scaling_columns])
 
-    df_scaled = pd.DataFrame(df_scaled, columns=scaling_data)
+    df_scaled = pd.DataFrame(df_scaled, columns=scaling_columns)
     
     df.update(df_scaled)
 
@@ -48,7 +92,7 @@ def data_scarling(df: pd.DataFrame) -> pd.DataFrame:
 
 def preparing_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df.drop(columns=delete_columns)
-    
+    df = input_missing_values(df=df)
     df = categorical_encoding(df=df)
     df = data_scarling(df=df)
 
